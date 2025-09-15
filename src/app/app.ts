09f2b2b5339ebf22ca41f6d89bcd1f1f6008fcc1
Samuel Cardona <scardona0297@gmail.com>
2025-09-15 17:54:15 -0500

    Se implementaron librerias de graficos y se hizo la prueba creando 1 grafico cond atos estaticos, todo OK

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Graphics } from './components/graphics/graphics';
+import { BarChartComponent } from './components/bar-chart/bar-chart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, Graphics,BarChartComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('mac-indicadores');
---
+++ src/app/components/graphics/graphics.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics',
+  standalone: true,
+  imports: [],
+  templateUrl: './graphics.html',
+  styleUrls: ['./graphics.css']
+})
+export class Graphics {
+
+}
